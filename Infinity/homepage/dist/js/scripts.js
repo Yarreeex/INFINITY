98771874249c101b4e688033deda8905b18c2bf6
Yarrzeee <104670362+Yarreeex@users.noreply.github.com>
2023-05-15 13:17:36 +0700

    Add files via upload

--- Infinity/homepage/dist/js/scripts.js
+++ Infinity/homepage/dist/js/scripts.js
@@ -1,7 +1,57 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+let cartList = [];
+
+function addToCart(item, price) {
+  cartList.push({ item, price, checked: false });
+  updateCart();
+}
+
+function updateCart() {
+  let cartListHTML = '';
+  let cartTotal = 0;
+  let cartItems = 0;
+  
+  for (let i = 0; i < cartList.length; i++) {
+    let item = cartList[i];
+    let isChecked = '';
+    
+    if (item.checked) {
+      cartTotal += item.price;
+      isChecked = 'checked';
+    }
+    
+    cartListHTML += `
+      <li>
+        <div class="d-flex justify-content-between">
+          <div>
+            <span>${item.item} - IDR ${item.price}K</span>
+          </div>
+          <div class="checkbox-kotak">
+            <input class="form-check-input remove-item" type="checkbox" value="${i}" ${isChecked}>
+          </div>
+        </div>
+      </li>
+    `;
+    
+    cartItems += 1;
+  }
+  
+  document.querySelector('#cart-list').innerHTML = cartListHTML;
+  document.querySelector('.total-items').innerHTML = `Total Items: ${cartItems}`;
+  document.querySelector('.total-price').innerHTML = `Price: IDR ${cartTotal}K`;
+  document.querySelector('.badge').innerHTML = cartItems;
+}
+
+document.querySelector('#cart-list').addEventListener('change', function(event) {
+  if (event.target.classList.contains('remove-item')) {
+    const itemIndex = parseInt(event.target.value);
+    cartList[itemIndex].checked = event.target.checked;
+    updateCart();
+  }
+});
+
+document.querySelector('.reset-btn').addEventListener('click', function(event) {
+  cartList = cartList.filter(function(item) {
+    return !item.checked;
+  });
+  updateCart();
+});
